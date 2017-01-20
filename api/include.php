898ca8c1eefc6825b4260e26875bda9c7ce91124
Hardi Huang <Hardi Huang>
2017-01-21 00:00:56 +0800

    remove database password

--- api/include.php
+++ api/include.php
@@ -3,14 +3,10 @@ header("Content-Type: text/html; charset=utf8");
 date_default_timezone_set("PRC");
 session_start();
 
-define("DB_HOST","localhost");
-define("DB_USER","root");
-define("DB_PWD","");
-define("DB_DBNAME","hh_wiki");
-define("DB_CHARSET","utf8");
 
 require_once 'lib/mysql.func.php';
 require_once 'lib/upload.func.php';
 require_once 'lib/image.func.php';
 require_once 'lib/string.func.php';
+require_once 'lib/db.pass.php';
 connect();
